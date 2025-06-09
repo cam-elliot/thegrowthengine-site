293b33e9705cd41615ab6b2bd77685e2469ba8c1
cam-elliot <cam@thegrowthengine.com.au>
2025-06-09 22:14:54 +1000

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
+
 import Home from "@/pages/home";
 import AboutPage from "@/pages/about";
 import ServicesPage from "@/pages/services";
